3b771e2a74d5395043ef73c284f866dba0f3e3f8
Surya9122prakash <sspsurya2002@gmail.com>
2024-07-15 10:28:32 +0530

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/tailwindcss",
     'nuxt-vue3-google-signin',
     '@pinia/nuxt'
   ],
+
   googleSignIn: {
     clientId: process.env.GOOGLE_CLIENT_ID,
   },
+
   plugins: [
     '~/plugins/CartContext.js',
     '~/plugins/vue3-google-login.js'
   ],
+
   build: {
     transpile: ['vuex'],
   },
+
+  compatibilityDate: '2024-07-15',
 });
